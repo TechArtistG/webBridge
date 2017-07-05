7f43272557e16a8608a2e9e2f6c3c4b865550ce2
rambutan2000 <geordiemoffatt@rocketmail.com>
2017-07-05 01:00:43 -0700

    Added first version of runtime interface def

--- webBridge.js
+++ webBridge.js
@@ -1,18 +1,16 @@
-/*(function( $wbi ) {
-    $wbi.m = {};
-    $wbi.m.name = "untitledInterface";
-    $wbi.p = {};
-    $wbi.f = {};
-    $wbi.fs = {};
-    $wbi.e = {};
-}( window.$wbi = window.$wbi || {}));*/
 "use strict";
-(function( $wbi ) {
-        
-    class i {        
+(function( wbi ) {
+    wbi.createInterface = function(objName, classDef) {  
+        wbi[objName] = new classDef;
+        wbi[objName]._iName = objName;        
+    }
+    
+    wbi.i = class {
         constructor() {
-            var STRIP_COMMENTS = /(\/\/.*$)|(\/\*[\s\S]*?\*\/)|(\s*=[^,\)]*(('(?:\\'|[^'\r\n])*')|("(?:\\"|[^"\r\n])*"))|(\s*=[^,\)]*))/mg;
-            this.ARGUMENT_NAMES = /([^\s,]+)/g;
+            this._iName = "";
+            this._StripComments = /(\/\/.*$)|(\/\*[\s\S]*?\*\/)|(\s*=[^,\)]*(('(?:\\'|[^'\r\n])*')|("(?:\\"|[^"\r\n])*"))|(\s*=[^,\)]*))/mg;
+            this._ArgNames = /([^\s,]+)/g;
+            this._MethodFilter = ["constructor", "hasOwnProperty", "valueOf", "toString", "toLocaleString", "isPrototypeOf", "propertyIsEnumerable"];
         }
         
         _callSyncFunction(fnName, args) {
@@ -28,28 +26,84 @@
             
         }
         
-        _getParamNames(func) {
-          var fnStr = func.toString().replace(STRIP_COMMENTS, '');
-          var result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+        _getMethodParamNames(func) {
+          var fnStr = func.toString().replace(this._StripComments, '');
+          var result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(this._ArgNames);
           if(result === null)
              result = [];
-          return result;
+          return(result);
         }
         
-        _getAllMethodNames() {
+        _getMethodNames() {
             var obj = this;
+            var self = this;
             let methods = new Set();
             while (obj = Reflect.getPrototypeOf(obj)) {
-            let keys = Reflect.ownKeys(obj)
-            keys.forEach((k) => methods.add(k));
+            let keys = Reflect.ownKeys(obj)            
+            keys.forEach(function(k) {            	
+            	if(self._MethodFilter.indexOf(k) == -1 && !k.startsWith("_")) {
+              	methods.add(k);
+              }
+            });
           }
-          return methods;
+          return(methods);
+        }
+        
+        _getPropertyNames() {
+            var props = [];
+            for(var propName in this) {
+            		if(!propName.startsWith("_")) {
+                	props.push(propName);
+                }                            
+            }            
+            return(props);
         }
         
         _getInterface() {
-         
+            var self = this;
+            var retObj = {};
+            retObj.interface = this._iName;
+            
+            // Props
+            retObj.props = [];
+            self._getPropertyNames().forEach(function(p) { retObj.props.push(p) });
+            
+            // Methods
+            retObj.methods = [];
+            self._getMethodNames().forEach(function(m) { 
+                var mObj = {};
+                mObj.name = m;
+                mObj.args = self._getMethodParamNames(self[m]);
+                retObj.methods.push(mObj);
+            });
+            return(retObj);
         }
     }
-    $wbi.i = new(i);
     
-}( window.$wbi = window.$wbi || {}));
+    
+    wbi.test_class = class extends wbi.i{
+    	constructor() {
+      	super();
+      	this.testprop = 0;
+      }
+      
+      __testFnA() {return({
+          comment: "This is a test method",
+          returnType: "string",
+          args:[
+              {name: "argA", type:"string"},
+              {name: "argB", type:"number"}
+          ]
+      })};
+      testFnA(argA, argB) {
+      	
+      }
+      
+      testFnB(argA, argB) {
+      	
+      }
+    }
+    wbi.createInterface("test", wbi.test_class);
+    //this.test = new(wbi.test_class);
+    
+}( window.wbi = window.wbi || {}));
